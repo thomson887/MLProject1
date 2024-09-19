71e025cbfb7772e4f8447b6c78e9388cf3067bac
Justin Thomson <justinthomson2019@gmail.com>
2024-09-19 12:42:01 -0500

    Implemented Grid Search in the model_trainer.py and utils.py file.

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -21,6 +21,7 @@ from src.logger import logging
 
 from src.utils import save_object, evaluate_models
 
+
 @dataclass
 class ModelTrainerConfig:
     trained_model_file_path = os.path.join("artifacts", "model.pkl")
@@ -41,24 +42,60 @@ class ModelTrainer:
             models = {
                 "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
-                "Gradienr Boosting": GradientBoostingRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
                 "Linear Regression": LinearRegression(),
-                "K-Neighbors Classifier": KNeighborsRegressor(),
+                "K-Neighbors Regressor": KNeighborsRegressor(),
                 "XGBoost Regressor": XGBRegressor(),
-                "Cat Boost Regressor": CatBoostRegressor(),
+                "Cat Boost Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor()
+            }
+
+            params = {
+                "Decision Tree":{
+                    'max_depth': [10, 20, 30, None],
+                    'min_samples_split': [2, 5, 10],
+                    'min_samples_leaf': [1, 2, 4]
+                },
+                "Random Forest":{
+                    'n_estimators': [8,16,32,64,128,256], #number of trees in the forest.
+                    #'max_features': ['auto', 'sqrt'], #number of features to consider at every split.
+                    #'max_depth': [int(x) for x in np.linspace(10,110,num=11)]
+                    #max.depth.append(None)                    
+                },
+                "Gradient Boosting":{
+                    'n_estimators': [50, 100, 200],
+                    'learning_rate': [0.01, 0.1, 0.2],
+                    'max_depth': [3, 5, 7],
+                },
+                "Linear Regression":{},
+                "K-Neighbors Regressor":{
+                    'n_neighbors':[5,7,9,11]
+                },
+                "XGBoost Regressor":{
+                    'learning_rate':[0.1,0.01,0.05,0.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Cat Boost Regressor":{
+                    'depth':[6,8,10],
+                    'learning_rate':[0.01,0.05,0.1],
+                    'iterations':[30,50,100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[0.1,0.01,0.5,0.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                }
             }
 
             #Creating a dictionary of the data set and models
             model_report:dict=evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test,
-                                            models=models)
+                                            models=models, param=params)
             
             #To get best model score from dict
-            best_model_score = max(sorted(model_report.values()))
+            best_model_score = max([score[0] for score in model_report.values()])
 
             #To get best model name from dict
             best_model_name = list(model_report.keys())[
-                list(model_report.values()).index(best_model_score)
+                list(score[0] for score in model_report.values()).index(best_model_score)
             ]
             best_model = models[best_model_name]
 
@@ -68,14 +105,12 @@ class ModelTrainer:
 
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
-                obj=best_model_name
+                obj=best_model
             )
 
             predicted = best_model.predict(X_test)
             r2_square = r2_score(y_test, predicted)
             return r2_square
-
-
 
         except Exception as e:
             raise CustomException(e,sys)
